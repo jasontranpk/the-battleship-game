856b57af365df93d7463dcb5effbb4434373ccf9
Jason <jasontran.pk@gmail.com>
2022-05-17 10:56:52 +0700

    crafting interface

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import './assets/css/style.css';
 import ship from './ship';
 
-const myShip = ship(6, { x: 2, y: 3 });
-myShip.hit(0);
-myShip.hit(1);
-myShip.hit(2);
-myShip.hit(3);
-myShip.hit(4);
-console.log(myShip);
